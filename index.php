84646c8892c78b39c331eec5578d42af7d831159
Max Revitt <max.revitt@gmail.com>
2012-11-05 22:16:01 +0000

    brap

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
 
+require 'vendor/autoload.php';
+
+$app = new \Slim\Slim(array('debug' => true));
+$dave = function($id) use ($app) {
+    var_dump($app);
+};
+
+$app->get('/:id', function($id) use ($app) {
+    var_dump($id);
+});
+
+$app->run();
+
 ?>
