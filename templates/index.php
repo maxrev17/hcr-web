c5062c3b7adaa83fa16d2f3a4a7df7f4c1a4d68e
Max Revitt <max.revitt@gmail.com>
2012-11-08 21:00:51 +0000

    test link online

--- templates/index.php
+++ templates/index.php
@@ -5,6 +5,8 @@
     <div class="container">
 
       <h1>Thanks for visiting :)!</h1>
+      
+      <a href="icrobot.net/c4tha7k">Sample shortcode</a>
           
     </div> <!-- /container -->
     
