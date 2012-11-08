51fc84f76b973ebb0d57efb99381c694567184dd
Max Revitt <max.revitt@gmail.com>
2012-11-08 18:29:56 +0000

    visual enhancements lol

--- templates/index.php
+++ templates/index.php
@@ -3,12 +3,21 @@
 <script language="JavaScript" src="includes/js/bootstrap.min.js"></script>
 </head>
 
-<?php
-echo '<h1>'.$title.'</h1>';
-echo '<br>';
-echo 'Your unique link code is ' . $visit_code . ', thanks for clicking!';
-echo '<br>';
-echo 'You have viewed your link ' . $visit_count . ' times!';
-echo '<br>';
-echo 'You spoke with ROBOT at the following time, ' . $visit_date;
-?>
+<title>
+    <?php echo $title ?>
+</title>
+
+<body>    
+
+    <?php    
+        echo '<h1>'.$title.'</h1>';
+        echo '<br>';
+        echo 'Your unique link code is <strong>' . $visit_code . '</strong>, thanks for clicking!';
+        echo '<br>';
+        echo 'You have viewed your link <strong>' . $visit_count . '</strong> times!';
+        echo '<br>';
+        echo 'You spoke with ROBOT at the following time, ' . $visit_date;
+    ?>  
+
+</body>
+
