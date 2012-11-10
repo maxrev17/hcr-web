e8f139d4e756b04d8170caa98bc192c5932d5e82
Max Revitt <max.revitt@gmail.com>
2012-11-10 18:51:27 +0000

    imperial college london

--- templates/header.php
+++ templates/header.php
@@ -35,6 +35,9 @@
     <div class="navbar navbar-inverse navbar-fixed-top">
       <div class="navbar-inner">
         <div class="container">
+            <p class="navbar-text pull-right">
+              Imperial College London
+            </p>
           <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
