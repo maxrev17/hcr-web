ebab23686e115af03bac3b16fbf981b1f7c51a38
Max Revitt <max.revitt@gmail.com>
2012-11-12 19:10:59 +0000

    more on the frame stuff

--- templates/code.php
+++ templates/code.php
@@ -4,14 +4,15 @@
 
     <div class="container">
 
-      <h1>Thanks for visiting :)!</h1>
+      <h3>Thanks for clicking, we will donate 10p to Cancer Research on your behalf!</h3>
 
       <?php    
-        echo 'Your unique link code is <strong>' . $visit_code . '</strong>, thanks for clicking!';
-        echo '<br>';
-        echo 'You have viewed your link <strong>' . $visit_count . '</strong> times!';
-        echo '<br>';
-        echo 'You spoke with ROBOT at the following time, <strong>' . $visit_date . '</strong>';
+        //echo 'Your unique link code is <strong>' . $visit_code . '</strong>, thanks for clicking!';
+        //echo '<br>';
+        //echo 'You have viewed your link <strong>' . $visit_count . '</strong> times!';
+        //echo '<br>';
+        echo 'You spoke with ROBOT at the following time, <strong>' . $visit_date . '</strong><br>';
+        echo 'Please consider making regular donations to this great cause...'
       ?>
       
       <iframe src="http://supportus.cancerresearchuk.org/campaign-pages/donate/" seamless="seamless" height="1300" width="100%"></ifram>
