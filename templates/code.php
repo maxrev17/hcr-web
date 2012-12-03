8e6642be3c24b987e7c276cf1fdbfa86c3fce8c4
Max Revitt <max.revitt@gmail.com>
2012-12-03 01:01:17 +0000

    showing stats

--- templates/code.php
+++ templates/code.php
@@ -7,10 +7,10 @@
       <h3>Thanks for clicking!</h3>
 
       <?php    
-        //echo 'Your unique link code is <strong>' . $visit_code . '</strong>, thanks for clicking!';
-        //echo '<br>';
-        //echo 'You have viewed your link <strong>' . $visit_count . '</strong> times!';
-        //echo '<br>';
+        echo 'Your unique link code is <strong>' . $visit_code . '</strong>, thanks for clicking!';
+        echo '<br>';
+        echo 'You have viewed your link <strong>' . $visit_count . '</strong> times!';
+        echo '<br>';
         echo 'You spoke with ROBOT at the following time, <strong>' . $visit_date . '</strong><br>';
       ?>
       
