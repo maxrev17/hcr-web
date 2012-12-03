b3a23e8f9e6774a3874ea1cc4a83e845a29e6cf8
Max Revitt <max.revitt@gmail.com>
2012-12-03 19:08:27 +0000

    added robot page with images and descriptions

--- templates/code.php
+++ templates/code.php
@@ -9,8 +9,8 @@
       <?php    
         echo 'Your unique link code is <strong>' . $visit_code . '</strong>, thanks for clicking!';
         echo '<br>';
-        echo 'You have viewed your link <strong>' . $visit_count . '</strong> times!';
-        echo '<br>';
+        //echo 'You have viewed your link <strong>' . $visit_count . '</strong> times!';
+        //echo '<br>';
         echo 'You spoke with ROBOT at the following time, <strong>' . $visit_date . '</strong><br>';
       ?>
       
--- templates/robot.php
+++ templates/robot.php
@@ -3,7 +3,32 @@
 ?>
 
     <div class="container">
-        Statistics etc.
+      <div class="hero-unit">
+        <h1>CHARLES</h1>
+        <p>Centralised Human Action Robot Leaflet Effects System. </p>
+        <!--<p><a class="btn btn-primary btn-large">Learn more &raquo;</a></p>-->
+      </div>
+      <div class="row">
+        <div class="span4">
+          <h2>Purpose</h2>
+          <p style="height:120px;">CHARLES aims to interact with the general public in order to bring them to this site. CHARLES hands out tickets which have a 
+              link to the icrobot.net printed on them. Using a unique URL for each person CHARLES talks with, we will be able to see how successful he
+              is at encouraging people to follow the link on the ticket.</p>
+          <img class="img-polaroid" src="../includes/img/ticket_sample.jpg"/>
+        </div>
+        <div class="span4">
+          <h2>Testing Theories</h2>
+          <p style="height:120px;">CHARLES has several modes of interaction, ranging from standing still and printing tickets, right up to talking with people and 
+          encouraging them to take a ticket via conversation. The data collected from these interactions will be linked with the unique URL, so we can test which 
+          mode of interaction is most successful.</p>
+           <img class="img-polaroid" src="../includes/img/graph_sample.jpg"/>
+       </div>
+        <div class="span4">
+          <h2>Communication</h2>
+          <p style="height:120px;">In order to display emotion, CHARLES has a face which is able to display a variety of expressions. This is combined with an ability to 
+          speak using text-to-speech software. Together these systems allow CHARLES to conduct a 'human-like' interaction with members of the public.</p>
+          <img class="img-polaroid" src="../includes/img/face_debug.jpg"/>
+        </div>   
     </div> <!-- /container -->
     
 <?php
