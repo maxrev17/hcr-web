42e8e46e44f3562281a2745c05d45faa0e97ce2a
Max Revitt <max.revitt@gmail.com>
2012-12-11 00:17:12 +0000

    more on the images

--- templates/stats.php
+++ templates/stats.php
@@ -8,19 +8,6 @@
         <p>Centralised Human Action Robot Leaflet Effects System. </p>
         <!--<p><a class="btn btn-primary btn-large">Learn more &raquo;</a></p>-->
       </div>
-      <div class="row">
-        <div class="span4">
-          <h2>Purpose</h2>
-          <img class="img-polaroid" src="../includes/img/ticket_sample.jpg"/>
-        </div>
-        <div class="span4">
-          <h2>Testing Theories</h2>
-           <img class="img-polaroid" src="../includes/img/graph_sample.jpg"/>
-       </div>
-        <div class="span4">
-          <h2>Communication</h2>
-          <img class="img-polaroid" src="../includes/img/face_debug.jpg"/>
-        </div>   
         </div> <!-- container -->
     
 <?php
