12f2285c79abcad7781a59ff9bfdc86baf00c940
Max Revitt <max.revitt@gmail.com>
2012-12-12 23:49:15 +0000

    tidying up messy image sizes

--- templates/index.php
+++ templates/index.php
@@ -267,20 +267,20 @@
           <p style="height:120px;">CHARLES aims to interact with the general public in order to bring them to this site. CHARLES hands out tickets which have a 
               link to the icrobot.net printed on them. Using a unique URL for each person CHARLES talks with, we will be able to see how successful he
               is at encouraging people to follow the link on the ticket.</p>
-          <img class="img-polaroid" src="../includes/img/ticket_sample.jpg"/>
+          <img class="img-polaroid" src="../includes/img/ticket_sample_small.jpg" style="height:258px;"/>
         </div>
         <div class="span4">
           <h2>Testing Theories</h2>
           <p style="height:120px;">CHARLES has several modes of interaction, ranging from standing still and printing tickets, right up to talking with people and 
           encouraging them to take a ticket via conversation. The data collected from these interactions will be linked with the unique URL, so we can test which 
           mode of interaction is most successful.</p>
-           <img class="img-polaroid" src="../includes/img/graph_sample.jpg"/>
+           <img class="img-polaroid" src="../includes/img/graph_sample_small.jpg" style="height:258px;"/>
        </div>
         <div class="span4">
           <h2>Communication</h2>
           <p style="height:120px;">In order to display emotion, CHARLES has a face which is able to display a variety of expressions. This is combined with an ability to 
           speak using text-to-speech software. Together these systems allow CHARLES to conduct a 'human-like' interaction with members of the public.</p>
-          <img class="img-polaroid" src="../includes/img/face_debug.jpg"/>
+          <img class="img-polaroid" src="../includes/img/face_debug_small.jpg" style="height:258px;"/>
         </div>   
     </div> <!-- /container -->
     
