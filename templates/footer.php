9037ced99c923aa783f1d3488d29f40c14faf59a
Max Revitt <max.revitt@gmail.com>
2012-12-12 22:44:27 +0000

    google chart stats page

--- templates/footer.php
+++ templates/footer.php
@@ -22,5 +22,6 @@
 
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
     <script src="includes/js/bootstrap.min.js"></script>
+    
     </body>
 </html>
--- templates/stats.php
+++ templates/stats.php
@@ -3,20 +3,90 @@
 ?>
 
     <div class="container">
-      <div class="hero-unit">
-        <h1>CHARLES</h1>
-        <p>Centralised Human Action Robot Leaflet Effects System. </p>
-        <!--<p><a class="btn btn-primary btn-large">Learn more &raquo;</a></p>-->
-      </div>
-      
-      <div class="row">
-          <div class="span 4">
-            Site hits from tickets handed out: 
-            <strong><?php echo $site_hits; ?></strong>
-          </div>
-      </div>
-       
-      </div> <!-- container -->
+        <div class="page-header">
+            <h1>Experiment Results</h1>
+            <p>Hypothesis: </p>
+        </div>
+
+        <div class="row">
+            <div class="span12">
+                <h3>Quick Stats</h3>
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="span2 well">
+                <p>Total tickets handed out: <strong>150</strong></p>
+            </div>
+            <div class="span2 well">
+                <p>Site hits from tickets handed out: 
+                <strong>29</strong>
+                </p>
+            </div>
+            <div class="span2 well">
+                <p>Average conversion rate:
+                <strong>19%</strong>
+                </p>
+            </div>
+            <div class="span2 well">
+                <p>Hours spent doing field testing: 
+                <strong>7</strong>
+                </p>
+            </div>
+        </div>
+        
+        <hr>
+        
+        <div class="row">
+            <div class="span12">
+                <h3>Detailed Information</h3>
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="span4">
+                <p>
+                The bar chart shows the percentage of interactions between robot and human which lead to the human
+                visiting our site using the unique link printed on the ticket given to them by CHARLES. Each bar represents 
+                a different mode of interaction. In mode 0, CHARLES doesn't attempt to interact, he just prints out tickets. 
+                In mode 1, he will only print a ticket when someone is detected infront of him. In mode 2 CHARLES will announce himself
+                and track passers by, printing a ticket for the person he is tracking. In mode 3, CHARLES holds a full conversation with
+                someone ending in the printing of a ticket if the person is interested in finding out more information.
+                </p>
+            </div>
+            <div class="span8"
+                <div id="chart_div" style="width: 600px; height: 250px;"></div>
+            </div>
+        </div>
+        
+
+    </div> <!-- container -->
+    
+    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+    <script type="text/javascript">
+      google.load("visualization", "1", {packages:["corechart"]});
+      google.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Mode', 'Memorable Interactions'],
+          ['0',  6],
+          ['1',  4],
+          ['2',  24],
+          ['3',  28]
+        ]);
+
+        var options = {
+          title: 'Memorability of Interactions VS Interaction Mode',
+          hAxis: {title: 'Interaction Mode', titleTextStyle: {color: 'grey'}},
+          vAxis: {title: 'Percentage of Memorable Interactions', titleTextStyle: {color: 'grey'}}
+        };
+
+        var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
+    </script>
+
+    
     
 <?php
     require 'footer.php';
